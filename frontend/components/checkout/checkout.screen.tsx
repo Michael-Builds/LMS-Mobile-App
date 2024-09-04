bb9f4b003cc7272029d43c6b246c574459cf9434
Michael-Builds <michaelkpantramp@gmail.com>
2024-09-04 13:00:08 +0000

    Updated

--- frontend/components/checkout/checkout.screen.tsx
+++ frontend/components/checkout/checkout.screen.tsx
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import useCart from '@/hooks/cart/useCart'
+import { StyleSheet, Text, View } from "react-native"
+import React, { useState, useEffect } from "react"
+import useCart from "@/hooks/cart/useCart"
 
 export default function CheckoutScreen() {
-    const { cartItems, fetchCart } = useCart()
-    console.log( fetchCart)
-    
+  const { cartItems } = useCart()
+
+  useEffect(() => {
+    console.log("Our carts", cartItems)
+  })
+
   return (
     <View>
       <Text>CheckoutScreen</Text>
--- frontend/utils/Endpoints.ts
+++ frontend/utils/Endpoints.ts
@@ -1,6 +1,8 @@
-export const auth = "http://192.168.1.110:4000/auth/api";
-export const order = "http://192.168.1.110:4000/order/api";
-export const course = "http://192.168.1.110:4000/courses/api";
-export const cart = "http://192.168.1.110:4000/cart/api";
-export const analytics = "http://192.168.1.110:4000/analytics/api";
-export const layout = "http://192.168.1.110:4000/layout/api";
+export const domain = process.env.NODE_ENV === 'production' ? "www.lms.com" : "http://192.168.1.110:4000"
+
+export const auth = `${domain}/auth/api`;
+export const order = `${domain}/order/api`;
+export const course = `${domain}/courses/api`;
+export const cart = `${domain}/cart/api`;
+export const analytics = `${domain}/analytics/api`;
+export const layout = `${domain}/layout/api`;
